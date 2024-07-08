d7a5b99418afd3723b16013dd6c94eb8d713fa18
thai_hang <thaihang7697@gmail.com>
2024-07-08 16:01:28 +0700

    update bai tap

--- src/BT_ToantuvaDieukien/Toantu_dieukien.java
+++ src/BT_ToantuvaDieukien/Toantu_dieukien.java
@@ -3,6 +3,23 @@ package BT_ToantuvaDieukien;
 public class Toantu_dieukien {
     public static void main(String[] args) {
         int number = 100;
+        int n = 97;
+        //th1 n==number
+        if(n==number)
+            System.out.println("Giá trị bằng nhau");
+        else
+            System.out.println("Giá trị không bằng nhau");
 
+        //th2 n<number
+        if(n<number)
+        System.out.println("Giá trị n bé hơn number");
+        else
+        System.out.println("Giá trị so sánh không đúng");
+
+        //th3 n>number
+        if(n>number)
+            System.out.println("Giá trị n không lớn hơn number");
+        else
+            System.out.println("Giá trị so sánh không đúng");
     }
 }
