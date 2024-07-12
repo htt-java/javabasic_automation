3c302add59ccc80b41376cf506785fe1ddb7d619
thai_hang <thaihang7697@gmail.com>
2024-07-12 20:02:15 +0700

    update

--- src/BaiTap_VonglapforvaMang/vonglapvamang.java
+++ src/BaiTap_VonglapforvaMang/vonglapvamang.java
@@ -2,10 +2,9 @@ package BaiTap_VonglapforvaMang;
 
 public class vonglapvamang {
     public static void main(String[] args) {
-        int tong = 0;
-        for(int i=0; i<=50; i++){
-            tong+=i;
+        int a[] = new int[50];
+        for(int i=0; i<50; i+=2){
+            System.out.println(i);
         }
-        System.out.println("Tong = " +tong);
     }
 }
