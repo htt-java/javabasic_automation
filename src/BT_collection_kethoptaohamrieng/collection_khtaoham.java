aac5f3672ccb34ffc876562c703c899564f0a9ef
thai_hang <thaihang7697@gmail.com>
2024-07-13 00:16:31 +0700

    update

--- src/BT_collection_kethoptaohamrieng/collection_khtaoham.java
+++ src/BT_collection_kethoptaohamrieng/collection_khtaoham.java
@@ -1,7 +1,33 @@
 package BT_collection_kethoptaohamrieng;
 
+import java.util.ArrayList;
+
 public class collection_khtaoham {
     public static void main(String[] args) {
+        ArrayList<String> addthongtin = new ArrayList<>();
+
+        //thêm thông tin tin
+        addthongtin.add("Tên");
+        addthongtin.add("Tuổi");
+        addthongtin.add("Giới Tính");
+        addthongtin.add("Nơi cư trú");
+        for (int i=0; i<addthongtin.size(); i++){
+            System.out.println(addthongtin.get(i));
+        }
+
+        //trả về 1 bản sao của mảng
+        System.out.println("==============");
+        addthongtin.clone();
+        for (int i=0; i<addthongtin.size(); i++){
+            System.out.println(addthongtin.get(i));
+        }
+
+        //clear all các phần tử trong danh sách
+        System.out.println("==============");
+        addthongtin.clear();
+        for (int i=0; i<addthongtin.size(); i++){
+            System.out.println(addthongtin.get(i));
+        }
 
     }
 }
