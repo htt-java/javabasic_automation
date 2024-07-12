f7317d3b0aa0c9efd92caeb2a573a5590ed56d89
thai_hang <thaihang7697@gmail.com>
2024-07-12 11:50:14 +0700

    update bai tap

---
+++ src/BaiTap_VonglapforvaMang/vonglapvamang.java
@@ -0,0 +1,11 @@
+package BaiTap_VonglapforvaMang;
+
+public class vonglapvamang {
+    public static void main(String[] args) {
+        int tong = 0;
+        for(int i=0; i<=50; i++){
+            tong+=i;
+        }
+        System.out.println("Tong = " +tong);
+    }
+}
